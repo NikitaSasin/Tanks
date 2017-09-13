8a994c7c622cd4b77a7f93c027272d1d712aa191
Nikita Sasin <nikita.sasin@hiqo-solutions.com>
2017-09-13 17:36:50 +0300

    Set up a workspace

---
+++ gulpfile.js
@@ -0,0 +1,118 @@
+'use strict';
+
+var gulp = require('gulp');
+var stylus = require('gulp-stylus')
+var watch = require('gulp-watch');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var rigger = require('gulp-rigger');
+var imagemin = require('gulp-imagemin');
+var rimraf = require('rimraf');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var cssnano = require('gulp-cssnano');
+
+var path = {
+  dist: {
+    html: 'dist/',
+    js: 'dist/js/',
+    css: 'dist/css/',
+    img: 'dist/img/',
+  },
+  src: {
+    html: 'src/*.html',
+    js: 'src/js/main.js',
+    style: 'src/style/common.styl',
+    img: 'src/img/*.*',
+  },
+  watch: {
+    html: 'src/**/*.html',
+    js: 'src/js/**/*.js',
+    style: 'src/style/**/*.styl',
+    img: 'src/img/**/*.*',
+  },
+  clean: './dist'
+};
+
+var config = {
+  server: {
+    baseDir: './dist'
+  },
+  tunnel: true,
+  host: 'localhost',
+  port: 9000,
+  logPrefix: 'sasin'
+};
+
+gulp.task('webserver', function() {
+  browserSync(config);
+});
+
+gulp.task('clean', function(cb) {
+  rimraf(path.clean, cb);
+});
+
+gulp.task('html:dist', function() {
+  gulp.src(path.src.html)
+    .pipe(rigger())
+    .pipe(gulp.dest(path.dist.html))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('js:dist', function() {
+  gulp.src(path.src.js)
+    .pipe(rigger())
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.dist.js))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('style:dist', function() {
+  gulp.src(path.src.style)
+  .pipe(sourcemaps.init())
+  .pipe(stylus())
+  .pipe(cssnano())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(path.dist.css))
+  .pipe(reload({
+    stream: true
+  }));
+});
+
+gulp.task('img:dist', function() {
+  gulp.src(path.src.img)
+    .pipe(imagemin())
+    .pipe(gulp.dest(path.dist.img))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('dist', [
+  'html:dist',
+  'js:dist',
+  'style:dist',
+  'img:dist'
+]);
+
+gulp.task('watch', function() {
+  watch([path.watch.html], function(event, cb) {
+    gulp.start('html:dist');
+  });
+  watch([path.watch.style], function(event, cb) {
+    gulp.start('style:dist');
+  });
+  watch([path.watch.js], function(event, cb) {
+    gulp.start('js:dist');
+  });
+  watch([path.watch.img], function(event, cb) {
+    gulp.start('img:dist');
+  });
+});
+
+gulp.task('default', ['dist', 'webserver', 'watch']);
